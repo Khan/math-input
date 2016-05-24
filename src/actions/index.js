cddee7cf4dfd5748641f44c66406a9b9a35257e5
Charles Marsh <charlie@khanacademy.org>
2016-05-24 12:36:54 -0700

    Add support for callback to `configureKeypad`
    Summary: This will allow us to only animate the keypad into view after the keypad has been re-configured.
    
    Test Plan: Coming in the integration diff.
    
    Reviewers: benkomalo
    
    Reviewed By: benkomalo
    
    Subscribers: kevinb
    
    Differential Revision: https://phabricator.khanacademy.org/D27558

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,11 +16,26 @@ module.exports = {
         });
     },
 
-    configureKeypad: function(configuration) {
+    /**
+     * Configure the keypad with the provided configuration parameters,
+     * triggering the callback upon completion.
+     *
+     * See: `prop-types.js#keypadConfigurationPropType`.
+     */
+    configureKeypad: function(configuration, cb) {
         store.dispatch({
             type: 'ConfigureKeypad',
             configuration,
         });
+
+        // HACK(charlie): In Perseus, triggering a focus causes the keypad to
+        // animate into view and re-configure. We'd like to provide the option
+        // to re-render the re-configured keypad before animating it into view,
+        // to avoid jank in the animation. As such, we support passing a
+        // callback into `configureKeypad`. However, implementing this properly
+        // would require middleware, etc., so we just hack it on with
+        // `setTimeout` for now.
+        setTimeout(() => cb && cb());
     },
 
     setButtonHeightPx: function(buttonHeightPx) {
