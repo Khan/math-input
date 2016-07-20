95b36a549df1c0f3a2ca07b33682dda498d72313
Charles Marsh <charlie@khanacademy.org>
2016-07-19 21:32:13 -0700

    Fix long-press behavior
    Summary:
    I added an `options` parameter to this constructor but forgot to provide it in the main callsite.
    My mistake! (I did provide it in the tests, so the tests were passing...)
    
    Test Plan:
    - Open the keypad.
    - Type some math.
    - Press and hold on the backspace key.
        - Verify that multiple presses are triggered, such that all the math is deleted as you hold down.
    
    Auditors: shadaj

--- src/components/gesture-manager.js
+++ src/components/gesture-manager.js
@@ -73,7 +73,7 @@ class GestureManager {
             onTrigger: (id) => {
                 this.popoverStateMachine.onTrigger(id);
             },
-        }, disabledSwipeKeys, multiPressableKeys);
+        }, {}, disabledSwipeKeys, multiPressableKeys);
     }
 
     /**
