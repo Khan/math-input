70a8cf38655efddf5f0b96225d90fb56c8b55ac1
Charles Marsh <charlie@khanacademy.org>
2016-08-18 10:09:15 -0400

    Guard against window being undefined
    Test Plan: Cross fingers.
    
    Auditors: kevinb

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ const {DecimalSeparators} = require('./consts');
 // We expect `window.icu` to be exposed by the parent. When in doubt, we fall
 // back to a period.
 let decimalSeparator;
-if (window.icu &&
+if (typeof(window) !== 'undefined' && window.icu &&
         window.icu.getDecimalFormatSymbols().decimal_separator === ',') {
     decimalSeparator = DecimalSeparators.COMMA;
 } else {
