191a2d636d832e2042f46c2d7616479ead272670
Jared Forsyth <jared@jaredforsyth.com>
2016-09-13 09:30:43 -0600

    Add in icu-slim.js and some docs
    Summary:
    This will make it clearer the subset of functionality that we can depend on
    from localeplanet. `icu-slim.js` is only used in the testing HTML page, but
    it's useful to set expectations.
    
    Test Plan: Open index.html, all should work well
    
    Reviewers: charlie
    
    Reviewed By: charlie
    
    Differential Revision: https://phabricator.khanacademy.org/D30705

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 const {DecimalSeparators} = require('./consts');
 
 // We expect `window.icu` to be exposed by the parent. When in doubt, we fall
-// back to a period.
+// back to a period. We can only depend on a subset of what localeplanet
+// provides, however -- the things in `icu-slim.js` (there's a copy in ../lib/
+// for reference).
 let decimalSeparator;
 if (typeof window !== 'undefined' && window.icu &&
         window.icu.getDecimalFormatSymbols().decimal_separator === ',') {
