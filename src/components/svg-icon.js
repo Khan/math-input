908b9d3988390e12413904db8be4122e35af543e
Charles Marsh <charlie@khanacademy.org>
2017-05-06 20:28:06 -0400

    Remove React 14-specific SVG hacks
    Summary:
    Now that webapp runs React 15, these will no longer be necessary.
    
    Test Plan: Cross fingers.
    
    Auditors: kevinb

--- src/components/svg-icon.js
+++ src/components/svg-icon.js
@@ -3,7 +3,6 @@
  */
 
 const React = require('react');
-const ReactDOM = require('react-dom');
 
 const Iconography = require('./iconography');
 
@@ -11,25 +10,6 @@ const SvgIcon = React.createClass({
     propTypes: {
         color: React.PropTypes.string.isRequired,
         name: React.PropTypes.string.isRequired,
-    },
-
-    componentDidMount() {
-        this._addFillRule();
-    },
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.name !== this.props.name) {
-            this._addFillRule();
-        }
-    },
-
-    _addFillRule() {
-        // TODO(kevinb) remove this when we upgrade to React 15.
-        const node = ReactDOM.findDOMNode(this);
-        if (node instanceof SVGElement) {
-            const firstGroup = node.querySelector('g');
-            firstGroup.setAttributeNS(null, 'fill-rule', 'evenodd');
-        }
     },
 
     render() {
