c3f6dbaae806fb9d5e701314486127848eea69fe
Alex Lopatin <alex@khanacademy.org>
2017-12-04 19:42:14 -0800

    Add a fully externalized webpack config
    Summary:
    With the exception of performance-now (which is small, so we keep it
    bundled), this new build option relies on the outside environment to
    satisfy its dependencies.
    
    Test Plan:
    ```
    node_modules/webpack/bin/webpack.js --config webpack.config.externals.js
    ```
    
    Copy the resulting `math-input.js` file over to webapp,
    then succesfully use it there.
    
    Reviewers: kevinb
    
    Reviewed By: kevinb
    
    Differential Revision: https://phabricator.khanacademy.org/D41138

---
+++ webpack.config.externals.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'math-input.js',
+        library: 'math-input',
+        libraryTarget: 'umd',
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+        }),
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel'],
+                exclude: /(node_modules|mathquill)/,
+            },
+        ],
+    },
+    // TODO(alex): Pick just one type below, e.g. commonjs2
+    externals: {
+        'react': {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+        },
+        'react-addons-css-transition-group': {
+            commonjs: 'react-addons-css-transition-group',
+            commonjs2: 'react-addons-css-transition-group',
+            amd: 'react-addons-css-transition-group',
+        },
+        'react-redux': {
+            commonjs: 'react-redux',
+            commonjs2: 'react-redux',
+            amd: 'react-redux',
+        },
+        'redux': {
+            commonjs: 'redux',
+            commonjs2: 'redux',
+            amd: 'redux',
+        },
+        'aphrodite': {
+            commonjs: 'aphrodite',
+            commonjs2: 'aphrodite',
+            amd: 'aphrodite',
+        },
+        'jquery': {
+            commonjs: 'jquery',
+            commonjs2: 'jquery',
+            amd: 'jquery',
+        },
+        'mathquill': {
+            commonjs: 'mathquill',
+            commonjs2: 'mathquill',
+            amd: 'mathquill',
+        },
+    },
+};
