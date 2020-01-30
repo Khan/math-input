aee4a205aacd2398ccd6df269d2abba76bfaf7c7
David Reid <david@stairwaylearning.com>
2020-01-30 11:45:37 +0000

    Expose math input state globally
    - Read the input's current value from `window.mathInputValue`.
    - Set it using `window.setMathInputValue`.

--- src/components/app.js
+++ src/components/app.js
@@ -13,12 +13,30 @@ class App extends React.Component {
         keypadType: consts.KeypadTypes.EXPRESSION,
     };
 
-    handleChange = (e) => {
+    componentDidMount = () => {
+        window.setMathInputValue = this.setValue;
+    }
+
+    setValue = (value, callback) => {
+        window.mathInputValue = value;
+        if (callback) {
+            this.setState({value}, callback);
+        } else {
+            this.setState({value});
+        }
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        if (prevState.keypadElement !== this.state.keypadElement) {
+            this.configureKeypad();
+        }
+    }
+
+    configureKeypad = () => {
         this.state.keypadElement.configure({
-            keypadType: e.target.value,
+            keypadType: this.state.keypadType,
             extraKeys: ["x", "y", "PI", "THETA"],
         });
-        this.setState({keypadType: e.target.value});
     };
 
     render() {
@@ -27,24 +45,12 @@ class App extends React.Component {
                 <KeypadInput
                     value={this.state.value}
                     keypadElement={this.state.keypadElement}
-                    onChange={(value, cb) => this.setState({value}, cb)}
+                    onChange={(value, callback) => {
+                        this.setValue(value, callback);
+                    }}
                     onFocus={() => this.state.keypadElement.activate()}
                     onBlur={() => this.state.keypadElement.dismiss()}
                 />
-                <View style={styles.selectContainer}>
-                    Keypad type: 
-                    <select 
-                        onChange={this.handleChange}
-                        value={this.state.keypadType}
-                    >
-                        <option value={consts.KeypadTypes.FRACTION}>
-                            FRACTION
-                        </option>
-                        <option value={consts.KeypadTypes.EXPRESSION}>
-                            EXPRESSION
-                        </option>
-                    </select>
-                </View>
             </View>
             <Keypad
                 onElementMounted={node => {
