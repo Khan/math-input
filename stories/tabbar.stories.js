80e59d8331d7320d5878bf27e3381b7a0c92efe6
Nicholas Rowe <nicholas@khanacademy.org>
2020-02-28 14:14:36 -0700

    WIP

--- stories/tabbar.stories.js
+++ stories/tabbar.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {action} from "@storybook/addon-actions";
 
-import {TabbarItem} from "../src/components/tabbar";
+import {Tabbar, TabbarItem} from "../src/components/tabbar";
 import {Button} from "@storybook/react/demo";
 
 export default {title: "Tab Bar"};
@@ -9,10 +9,17 @@ export default {title: "Tab Bar"};
 export const InactiveBarItem = () => (
     <TabbarItem
         itemState="inactive"
+        itemType="Geometry"
         onPress={() => {
             alert("activated");
         }}
     />
 );
-export const ActiveBarItem = () => <TabbarItem itemState="active" />;
-export const DisabledBarItem = () => <TabbarItem itemState="disabled" />;
+export const ActiveBarItem = () => (
+    <TabbarItem itemType="Geometry" itemState="active" />
+);
+export const DisabledBarItem = () => (
+    <TabbarItem itemType="Geometry" itemState="disabled" />
+);
+
+export const FullTabbar = () => <Tabbar></Tabbar>;
