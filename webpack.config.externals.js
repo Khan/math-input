0b823518baaebad42b83cdf65c69176b700b2a64
Kevin Barabash <kevinb@khanacademy.org>
2021-12-08 10:23:49 -0500

    rework deps so that we don't have conflicting versions of react

--- webpack.config.externals.js
+++ webpack.config.externals.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const terserOptions = require("./terser.js");
 
-const externals = [
+const externalsNames = [
     "prop-types",
     "react",
     "react-dom",
@@ -22,6 +22,17 @@ const externals = [
     "react-router",
     "react-router-dom",
 ];
+
+const externals = {};
+
+for (const name of externalsNames) {
+    // TODO(alex): Pick just one type below, e.g. commonjs2
+    externals[name] = {
+        commonjs: name,
+        commonjs2: name,
+        amd: name,
+    };
+}
 
 module.exports = {
     entry: "./src/index.js",
@@ -61,87 +72,5 @@ module.exports = {
             filename: "math-input.css",
         }),
     ],
-    // TODO(alex): Pick just one type below, e.g. commonjs2
-    externals: {
-        "prop-types": {
-            commonjs: "prop-types",
-            commonjs2: "prop-types",
-            amd: "prop-types",
-        },
-        react: {
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "react",
-        },
-        "react-dom": {
-            commonjs: "react-dom",
-            commonjs2: "react-dom",
-            amd: "react-dom",
-        },
-        "react-transition-group": {
-            commonjs: "react-transition-group",
-            commonjs2: "react-transition-group",
-            amd: "react-transition-group",
-        },
-        "react-redux": {
-            commonjs: "react-redux",
-            commonjs2: "react-redux",
-            amd: "react-redux",
-        },
-        redux: {
-            commonjs: "redux",
-            commonjs2: "redux",
-            amd: "redux",
-        },
-        aphrodite: {
-            commonjs: "aphrodite",
-            commonjs2: "aphrodite",
-            amd: "aphrodite",
-        },
-        "flow-enums-runtime": {
-            commonjs: "flow-enums-runtime",
-            commonjs2: "flow-enums-runtime",
-            amd: "flow-enums-runtime",
-        },
-        jquery: {
-            commonjs: "jquery",
-            commonjs2: "jquery",
-            amd: "jquery",
-        },
-        mathquill: {
-            commonjs: "mathquill",
-            commonjs2: "mathquill",
-            amd: "mathquill",
-        },
-        katex: {
-            commonjs: "katex",
-            commonjs2: "katex",
-            amd: "katex",
-        },
-        "@khanacademy/wonder-blocks-clickable": {
-            commonjs: "@khanacademy/wonder-blocks-clickable",
-            commonjs2: "@khanacademy/wonder-blocks-clickable",
-            amd: "@khanacademy/wonder-blocks-clickable",
-        },
-        "@khanacademy/wonder-blocks-core": {
-            commonjs: "@khanacademy/wonder-blocks-core",
-            commonjs2: "@khanacademy/wonder-blocks-core",
-            amd: "@khanacademy/wonder-blocks-core",
-        },
-        "@khanacademy/wonder-blocks-color": {
-            commonjs: "@khanacademy/wonder-blocks-color",
-            commonjs2: "@khanacademy/wonder-blocks-color",
-            amd: "@khanacademy/wonder-blocks-color",
-        },
-        "react-router": {
-            commonjs: "react-router",
-            commonjs2: "react-router",
-            amd: "react-router",
-        },
-        "react-router-dom": {
-            commonjs: "react-router-dom",
-            commonjs2: "react-router-dom",
-            amd: "react-router-dom",
-        },
-    },
+    externals: externals,
 };
